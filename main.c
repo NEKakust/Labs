cec38793e9d50ded81661f168f58a767f4f0902c
NEKakust <70840485+NEKakust@users.noreply.github.com>
2024-08-29 22:17:13 +0300

    Memory usage of vector

--- main.c
+++ main.c
@@ -1,2 +1,11 @@
-//
+#include <stdio.h>
+#include <stdint.h>
+#include "libs/data_structures/vector/vector.h"
 
+int main (){
+    vector vector1;
+    
+    reserve(&vector1,12);
+
+    return 0;
+};
