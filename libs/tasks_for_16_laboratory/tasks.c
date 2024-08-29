d2bce3a0f05e58a52c74c010cb0c06e8f5a0d21e
NEKakust <70840485+NEKakust@users.noreply.github.com>
2024-08-30 00:54:25 +0300

    Add 5, 6, 7, 8 tasks

--- libs/tasks_for_16_laboratory/tasks.c
+++ libs/tasks_for_16_laboratory/tasks.c
@@ -10,3 +10,51 @@ void task1(matrix m) {
 void task2(matrix m) {
     insertionSortRowsMatrixByRowCriteria(m, getMax);
 }
+
+
+void task3(matrix m){
+    selectionSortColsMatrixByColCriteria(m, getMin);
+}
+
+
+void task4(matrix *m){
+    if (isSymmetricMatrix(m)){
+        getSquareOfMatrix(m);
+    }
+}
+
+void task5(matrix m){
+    if (isSquareMatrix(&m)){
+        int arraySum[m.nRows];
+        for (int ind = 0; ind < m.nRows; ind++){
+            arraySum[ind] = getSum(m.values[ind], m.nCols);
+        }
+
+        if (isUnique(arraySum, m.nRows)){
+            transposeSquareMatrix(&m);
+        }
+    }
+}
+
+
+bool task6(matrix m1, matrix m2){
+    if (m1.nRows != m2.nRows || m1.nCols != m2.nCols){
+        return false;
+    }
+
+    bool flagIsMutuallyInverseMatrices = isMutuallyInverseMatrices(m1, m2);
+
+    return flagIsMutuallyInverseMatrices;
+}
+
+
+long long task7(matrix m){
+    return findSumOfMaxesOfPseudoDiagonal(m);
+}
+
+
+int task8(matrix m, int arrayLengthsShadedColumns[]){
+    int minNum = getMinInArea(m, arrayLengthsShadedColumns);
+
+    return minNum;
+}
