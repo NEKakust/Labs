6f3559c3d08955d0eaeb412b2207080538336efe
NEKakust <70840485+NEKakust@users.noreply.github.com>
2024-08-30 00:32:50 +0300

    Refactor function in matrix

---
+++ libs/algorithms/testing/testing.h
@@ -0,0 +1,11 @@
+
+#ifndef LAB13_TESTING_H
+#define LAB13_TESTING_H
+
+void hand_bitset_testing();
+void hand_ordered_set_testing();
+
+void auto_test_bitset();
+void auto_test_ordered_array();
+
+#endif //LAB13_TESTING_H
